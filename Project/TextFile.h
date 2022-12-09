7043bc5c10cbec6e1cc18a58166b5b8568f1576d
Owen <Ocovach4831@conestogac.on.ca>
2022-12-09 13:47:00 -0500

    Added comments

--- Project/TextFile.h
+++ Project/TextFile.h
@@ -1,7 +1,12 @@
+// Header File for text file functions
+// including header files and prototypes
+// 
+// Assignment 6 - Project
+// Owen Covach, Sudhan Dahake, Hangsihak Sin - prog71985 - fall 2022
+
+
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
 #include "ADT.h"
 
 void SaveAptToDisk(APPOINTMENT**);
-
+void LoadAptToProgram(APPOINTMENT**);
